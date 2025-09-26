62e50a5343e1aa063d5a4219d423aa30679aefa1
MicheleFumi <michele.fumi96@gmail.com>
2025-09-26 15:34:44 +0200

    fixata home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { CommonModule,   NgClass } from '@angular/common';
+import { CommonModule, NgClass } from '@angular/common';
 import { provideHttpClient, withFetch } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
