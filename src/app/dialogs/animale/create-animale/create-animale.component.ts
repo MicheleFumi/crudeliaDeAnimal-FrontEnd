3f7294f633bd221e28d56a7e4c56b87728c18408
micheleboanno92 <mikelebon7@gmail.com>
2025-09-28 11:20:54 +0200

    fatto form create

--- src/app/dialogs/animale/create-animale/create-animale.component.ts
+++ src/app/dialogs/animale/create-animale/create-animale.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AnimaliService } from '../../../services/animali.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-animale',
@@ -10,11 +13,34 @@ export class CreateAnimaleComponent implements OnInit {
 
   userId: number=Number(localStorage.getItem('idUtente'));
   tipi = ['cane', 'gatto', 'uccello', 'roditore', 'pesce', 'rettile'];
-
-  constructor() { }
+  form!: FormGroup;   
+  msg: string='';
+  constructor(
+    private service: AnimaliService,
+    private router: Router
+  ) {}
+  
 
   ngOnInit(): void {
-    
+    this.form = new FormGroup({ 
+      nomeAnimale: new FormControl ('', Validators.required),
+      razza: new FormControl ('', Validators.required),
+      tipo: new FormControl ('', Validators.required),
+      noteMediche: new FormControl ('', Validators.maxLength(500)),
+      utente : new FormControl ({id: this.userId})
+    })
   }
 
+  onSubmit() {
+    const params = this.form.value; 
+    console.log(params); 
+    this.service.create(params).subscribe((resp : any)=> {
+      if(resp.rc){
+        this.router.navigate(['animali/:id'])
+        window.location.reload()
+      }else{
+        this.msg = resp.msg 
+      }
+    })
+  }
 }
