612132d2d86044e3e02af781dfc34bcdf55b1f7c
micheleboanno92 <mikelebon7@gmail.com>
2025-09-28 09:49:56 +0200

    fix create

--- src/app/dialogs/animale/create-animale/create-animale.component.ts
+++ src/app/dialogs/animale/create-animale/create-animale.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-create-animale',
@@ -6,6 +6,15 @@ import { Component } from '@angular/core';
   templateUrl: './create-animale.component.html',
   styleUrl: './create-animale.component.css'
 })
-export class CreateAnimaleComponent {
+export class CreateAnimaleComponent implements OnInit {
+
+  userId: number=Number(localStorage.getItem('idUtente'));
+  tipi = ['cane', 'gatto', 'uccello', 'roditore', 'pesce', 'rettile'];
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
 
 }
